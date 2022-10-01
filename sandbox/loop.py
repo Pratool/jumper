173224343677fe967b03242205bb2b102a9bdb33
Pratool Gadtaula <5598110+Pratool@users.noreply.github.com>
2022-10-01 16:41:21 -0400

    Avoid game in global scope

--- sandbox/loop.py
+++ sandbox/loop.py
@@ -17,38 +17,41 @@ from typing import (
     Tuple
 )
 
+# Color constants
 WHITE: Tuple[int, int, int] = (255, 255, 255)
+BLACK: Tuple[int, int, int] = (0, 0, 0)
 
-# Run platform dependent intialization
-pygame.init()
+if __name__ == "__main__":
+    # Run platform dependent intialization
+    pygame.init()
 
-# Define screen resolution in pixels.
-SCREEN_WIDTH: int = 800
-SCREEN_HEIGHT: int = 600
+    # Define screen resolution in pixels.
+    SCREEN_WIDTH: int = 800
+    SCREEN_HEIGHT: int = 600
 
-# Set up the drawing window
-screen: pygame.surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # Set up the drawing window
+    screen: pygame.surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-# Run until the user asks to quit
-running: bool = True
+    # Run until the user asks to quit
+    running: bool = True
 
-while running:
-    # Did the user click the window close button?
-    for event in pygame.event.get():
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
+    while running:
+        # Did the user click the window close button?
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+            elif event.type == pygame.QUIT:
                 running = False
-        elif event.type == pygame.QUIT:
-            running = False
 
-    # Fill the background with white
-    screen.fill(WHITE)
+        # Fill the background with white
+        screen.fill(BLACK)
 
-    # dRAw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+        # dRAw a solid blue circle in the center
+        pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
 
-    # Flip the display
-    pygame.display.flip()
+        # Flip the display
+        pygame.display.flip()
 
-# Done! Time to quit.
-pygame.quit()
+    # Done! Time to quit.
+    pygame.quit()
